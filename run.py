c34666ccaf09bd63bd868abec739a7f2814392fe
Andrew Zhu <mommothazaz123@gmail.com>
2017-06-22 11:12:47 -0700

    things

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ class Program:
     def move_random(self, max_move_dist):
         a = round(max_move_dist/(20*3))
         a = min(3, a)
-        a = 2
+        a = 0
         self.robot.move(random.randint(-a, a), random.randint(-a, a))
 
 p = Program()
@@ -50,8 +50,11 @@ class Controller:
         self.robot = robot
         self.weights = [[0, 0], [0, 0]] # [[o1 on p1, o1 on p2], [o2 on p1, o2 on p2]]
         self.learned = [[0, 0], [0, 0]]
-        self.learn_rate = 0.005
+        self.learn_rate = 0.0005
+        self.old_learn = self.learn_rate
+        self.learn_rate_rate = 0.01
         self.last_output = None
+        self.last_lr_output = 0
     
     def get_delta(self, axis):
         """Returns a tuple."""
@@ -64,7 +67,7 @@ class Controller:
         hasWeight = True
         axis = None
         for a, w in enumerate(self.learned[control]):
-            hasWeight = w > 50
+            hasWeight = w > 20
             if not hasWeight:
                 axis = a
                 break
@@ -85,15 +88,36 @@ class Controller:
         
     def learn(self, before, after):
         print(self.last_output)
-        lc = 0.5
+        lc = 5
         for axis in (0, 1):
             for control in (0, 1):
-                self.weights[control][axis] += -self.learn_rate * (self.last_output[control] * (after[axis] - before[axis]) * PROGRAM_SPEED)
+                delta = abs(after[axis]) - abs(before[axis])
+                o = self.last_output[control]
+                if p.target[axis] > self.robot.getPos()[axis]: o = -o
+                if not abs(delta) == abs(self.last_output[control]): o = -abs(o)
+                a = -self.learn_rate * (o * delta * PROGRAM_SPEED)
+                self.weights[control][axis] += a
                 self.weights[control][axis] = max(-lc, min(lc, self.weights[control][axis]))
         
     def output(self, x, y):
+        x = min(10, max(-10, x))
+        y = min(10, max(-10, y))
         self.last_output = (x, y)
         self.robot.move(x, y)
+        
+    def change_learn(self):
+        out = random.randint(-5, 5) * self.get_delta_total() / 100000
+        self.last_lr_output += out
+        self.learn_rate += out
+        self.learn_rate = max(0, min(0.5, self.learn_rate))
+        
+    def learn_to_learn(self, before, after):
+        delta = abs(after) - abs(before)
+        if delta == 0: delta += 0.01
+        self.learn_rate = (self.old_learn) + -self.learn_rate_rate * (self.last_lr_output * delta * (PROGRAM_SPEED*10))
+        self.learn_rate = max(0, min(0.5, self.learn_rate))
+        self.old_learn = self.learn_rate
+        self.last_lr_output = 0
 
 def init():
     p.win = GraphWin("2D Point", X_MAX, Y_MAX, autoflush=False) #500x500 window
@@ -106,6 +130,8 @@ def init():
     
     p.tc = Circle(t, 5) # put target somewhere random
     p.tc.draw(p.win)
+    
+    p.score = 0
     
     #zero = Line(Point(0, 250), Point(500, 250))
     #zero.draw(p.graphwin)
@@ -138,34 +164,58 @@ def update_graphs():
             li.setFill(colors[i])
             li.draw(p.graphwin)
             p.datalines.append(li)
+            
+def update_target():
+    if p.cont.get_delta_total() < 2:
+        a = Point(random.randint(X_MIN, X_MAX), random.randint(Y_MIN, Y_MAX))
+        p.tc.move(a.x - p.target[0], a.y - p.target[1])
+        p.target = (a.x, a.y)
+        p.score += 1
 
 def loop(iteration):
+    b = Point(p.robot.getPos()[0], p.robot.getPos()[1])
     for control in (0, 1):
         before = (p.cont.get_delta(0), p.cont.get_delta(1))
         p.cont.move_robot(control)
-        if iteration > 30: # give it 3 cycles to learn
+        if iteration > 150: # give it 3 cycles to learn
             p.move_random(iteration)
         after = (p.cont.get_delta(0), p.cont.get_delta(1))
         p.cont.learn(before, after)
+    a = Point(p.robot.getPos()[0], p.robot.getPos()[1])
+    l = Line(b, a)
+    l.draw(p.win)
+    
+    #print(p.cont.learn_rate)
+    #if iteration > 10:
+        #p.cont.change_learn()
+            
+        #if iteration % 200:
+            #p.cont.learn_to_learn(p.before_ten, p.cont.get_delta_total())
+            #p.before_ten = p.cont.get_delta_total()
     
     print(p.cont.weights)
-    print(p.cont.get_delta(0), p.cont.get_delta(1))
+    #print(p.cont.get_delta(0), p.cont.get_delta(1))
     
     #update_graphs()
     
+    update_target()
+    
     time.sleep(PROGRAM_SPEED)
-    a = p.win.checkMouse()
-    k = p.win.checkKey()
-    if a:
-        p.tc.move(a.x - p.target[0], a.y - p.target[1])
-        p.target = (a.x, a.y)
-    if k == 'r':
-        p.robot.reverse = not p.robot.reverse
-    if k == 'q':
-        p.cont.weights = [[0, 0], [0, 0]]
-        p.cont.learned = [[0, 0], [0, 0]]
-    update()
-    return True
+    try:
+        a = p.win.checkMouse()
+        k = p.win.checkKey()
+        if a:
+            p.tc.move(a.x - p.target[0], a.y - p.target[1])
+            p.target = (a.x, a.y)
+        if k == 'r':
+            p.robot.reverse = not p.robot.reverse
+        if k == 'q':
+            p.cont.weights = [[0, 0], [0, 0]]
+            p.cont.learned = [[0, 0], [0, 0]]
+        update()
+    except GraphicsError:
+        return False
+    return iteration < 60*20 # 1 min
 
 if __name__ == '__main__':
     init()
